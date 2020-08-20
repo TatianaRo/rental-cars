ea1fff89ffd1ece4f115ea5ba197b6b6b0841d19
TatianaRo <tatianavln2012@gmail.com>
2020-08-20 15:15:10 -0300

    Correções no teste sign in

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -23,19 +23,19 @@ feature 'User sign in' do
         visit root_path
         click_on 'Entrar'
         fill_in 'Email', with: "joao@email.com"
-        fill_in 'Email', with: "joao@email.com"
+        fill_in 'Senha', with: "12345678"
         click_on 'Entrar'
 
 
         #Assert
         expect(page).to have_content 'João Almeida'
-        expect(page).to have_content 'Usuário Autenticado'
-        expect(page).to have_Link 'Sair'
-        expect(page).not_to have_Link 'Entrar'
+        expect(page).to have_content 'Login efetuado com sucesso'
+        expect(page).to have_link 'Sair'
+        expect(page).not_to have_link 'Entrar'
         
     end
 
-    xscenario 'and sign out' do
+    scenario 'and sign out' do
         user = User.create!(name: 'João Almeida', email:'joao@email.com', 
             password: '12345678')
 
@@ -45,8 +45,8 @@ feature 'User sign in' do
 
         expect(current_path).to eq root_path
         expect(page).to have_content 'Saiu com sucesso.'
-        expect(page).to have_content 'Entrar'
-        expect(page).not_to have_content 'Sair'
+        expect(page).to have_link 'Entrar'
+        expect(page).not_to have_link 'Sair'
                 
     end
 end
